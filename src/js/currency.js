dbbc7404e2692998517397d46ec25985233a3bd3
Isaac Moreno <ipdxcreative@gmail.com>
2021-04-23 10:45:22 -0700

    remove semicolon that made lines 4-14 unreadable

--- src/js/currency.js
+++ src/js/currency.js
@@ -1,6 +1,6 @@
 export default class CurrencyAPI {
   static convertCurrency() {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
       .then(function (response) {
         if (!response.ok) {
           throw Error(response.statusText);
@@ -10,5 +10,6 @@ export default class CurrencyAPI {
       .catch(function (error) {
         return error;
       });
+
   }
-} 
+}
