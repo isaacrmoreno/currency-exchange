a9e052eaa46b772c4682c9932a2718be779a5b14
Isaac Moreno <ipdxcreative@gmail.com>
2021-04-23 10:44:42 -0700

    add import statements to utilize bootstrap

--- src/main.js
+++ src/main.js
@@ -1,2 +1,9 @@
-import $ from 'jquery';
 import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import $ from 'jquery';
+import CurrencyAPI from './js/currency';
+
+$(document).ready(function () {
+  CurrencyAPI;
+});
