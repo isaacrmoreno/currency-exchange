721c387e1907bc57b2a35bfe0cc351bec9d373a0
Isaac Moreno <ipdxcreative@gmail.com>
2021-04-23 13:48:03 -0700

    switched to XMLHTTPRequest because I understand it better

--- src/main.js
+++ src/main.js
@@ -4,21 +4,50 @@ import './css/styles.css';
 import $ from 'jquery';
 import CurrencyAPI from './js/currency';
 
-function displayCurrencyConversion (response) {
-  if (response.main) { //response.main is unique to weather API. Response.body.myapi. 
-    $("#").text(`${somethingWIllGoHere}`);
-  } else {
-      $(".showErrors").text(`There was an error: ${response.message}`);
-    }
-  }
+$(document).ready(function() {
+  $('#convertCurrencyBtn').click(function(){
+    let inputtedCurrency = $('#inputtedCurrency').val();
+    $('#convertedCurrencyOutput').text(inputtedCurrency);
 
-$(document).ready(function () {
-  $("#convertCurrencyBtn").click(function() {
-    let currency = $("#currency").val();
-    clearFields();
-    CurrencyAPI.convertCurrency(currency);
-    .then(function (response) {
-      displayCurrencyConversion (response);
+    let currencyPromise = CurrencyAPI.convertCurrency(inputtedCurrency);
+    currencyPromise.then(function (response) {
+      const body = JSON.parse (response);
+      $("#convertCurrencyOutput").html(`${body.conversion_rates.inputtedCurrency}`);
+      return body;
+    }, function (error) {
+      console.error('request error ', error);
     });
   });
-}); 
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function displayCurrencyConversion (response) {
+//   if (response.main) { //response.main is unique to weather API. Response.body.myapi. 
+//     $("#").text(`${somethingWIllGoHere}`);m // Need to figure out what will go here? 
+//   } else {
+//       $(".showErrors").text(`There was an error: ${response.message}`);
+//     }
+//   }
+
+// $(document).ready(function () {
+//   $("#convertCurrencyBtn").click(function() {
+//     let currency = $("#currency").val(); // This wont work because its a drop down menu? 
+//     clearFields();
+//     CurrencyAPI.convertCurrency(currency);
+//     .then(function (response) {
+//       displayCurrencyConversion (response);
+//     });
+//   });
+// }); 
