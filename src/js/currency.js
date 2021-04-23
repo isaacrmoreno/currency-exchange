9b6a831b6ab04fe85f94ca61b9745d2b0c77f5e4
Isaac Moreno <ipdxcreative@gmail.com>
2021-04-23 13:48:22 -0700

    switched to XMLHTTPRequest because I understand it better

--- src/js/currency.js
+++ src/js/currency.js
@@ -1,16 +1,57 @@
 export default class CurrencyAPI {
-  static convertCurrency() {
-    let url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`; // potentially add template literal to "USD" so user can select any currency to exchange // Do that down the road when I get basic functionality. ON NEW BRANCH
-    return fetch(url)
-      .then(function (response) {
-        if (!response.ok) {
-          console.log(response);
-          throw Error(response.statusText);
+  static convertCurrency() { // put currency in here? 
+    return new Promise(function (resolve, reject) {
+      const apiKey = process.env.API_KEY;
+      let request = new XMLHttpRequest();
+      const url = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`;
+
+      request.onload = function () {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject (request.response);
         }
-        return response.json();
-      })
-      .catch(function (error) {
-        return error;
-      });
+      };
+      request.open('GET', url, true);
+      request.send();
+    });
   }
 }
+
+// Okay I think I know what I need to do. I need to change the inputted currency to be the dollaramount. Then I need to make a template literal for USD? 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// BELOW IS MY ATTEMPT AT FETCH @.@
+//   static convertCurrency() {
+//     let url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`; // potentially add template literal to "USD" so user can select any currency to exchange // Do that down the road when I get basic functionality. ON NEW BRANCH
+//     return fetch(url)
+//       .then(function (response) {
+//         if (!response.ok) {
+//           console.log(response);
+//           throw Error(response.statusText);
+//         }
+//         return response.json();
+//       })
+//       .catch(function (error) {
+//         return error;
+//       });
+//   }
+// }
+
