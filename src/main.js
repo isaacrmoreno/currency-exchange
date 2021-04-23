80f60a011057707f9c09871f5f7195dd4b7c8d26
Isaac Moreno <ipdxcreative@gmail.com>
2021-04-23 16:45:05 -0700

    final commit for the day

--- src/main.js
+++ src/main.js
@@ -7,20 +7,18 @@ import CurrencyAPI from './js/currency';
 $(document).ready(function() {
   $('#convertCurrencyBtn').click(function(){
     let dollarAmount = parseInt($('#dollarAmount').val()); 
-    $('#convertedCurrencyOutput').text(dollarAmount);
+    $('#convertedCurrencyOutput').text(dollarAmount); // This just returns the dollar amount. 
     
-    let selectedCurrency = parseInt($('#selectedCurrency').val());
+    let selectedCurrency = $('#selectedCurrency').val(); // console returns undefined. 
 
     let currencyPromise = CurrencyAPI.convertCurrency(selectedCurrency);
     currencyPromise.then(function (response) {
+
       const body = JSON.parse (response);
-      $("#convertCurrencyOutput").html(`${body}.conversion_rates.${selectedCurrency}`);
+      $("#convertCurrencyOutput").html(body.conversion_rates.selectedCurrency);
       return body;
     }, function (error) {
       console.error('request error ', error);
-    }); dollarAmount; // 
+    }); dollarAmount; 
   });
 });
-
-console.log(`${selectedCurrency}`); // place is returning NaN and thats because I gave it a value of string. Maybe I need to JsonParse somewhere else
-
