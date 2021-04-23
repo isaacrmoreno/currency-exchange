9533eac0ca0b9a66f541d6da3e2ab1230df819d6
Isaac Moreno <ipdxcreative@gmail.com>
2021-04-23 14:53:38 -0700

    remove const of apikey and just put it into const url

--- src/js/currency.js
+++ src/js/currency.js
@@ -1,9 +1,8 @@
 export default class CurrencyAPI {
-  static convertCurrency() { // put currency in here? 
+  static convertCurrency(selectedCurrency) { // put currency in here? 
     return new Promise(function (resolve, reject) {
-      const apiKey = process.env.API_KEY;
       let request = new XMLHttpRequest();
-      const url = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`;
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${selectedCurrency}`;
 
       request.onload = function () {
         if (this.status === 200) {
@@ -18,40 +17,4 @@ export default class CurrencyAPI {
   }
 }
 
-// Okay I think I know what I need to do. I need to change the inputted currency to be the dollaramount. Then I need to make a template literal for USD? 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// BELOW IS MY ATTEMPT AT FETCH @.@
-//   static convertCurrency() {
-//     let url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`; // potentially add template literal to "USD" so user can select any currency to exchange // Do that down the road when I get basic functionality. ON NEW BRANCH
-//     return fetch(url)
-//       .then(function (response) {
-//         if (!response.ok) {
-//           console.log(response);
-//           throw Error(response.statusText);
-//         }
-//         return response.json();
-//       })
-//       .catch(function (error) {
-//         return error;
-//       });
-//   }
-// }
 
