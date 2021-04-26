1941faa608f280ba63ceb09fd16e4279acb94c84
Isaac Moreno <ipdxcreative@gmail.com>
2021-04-25 19:55:44 -0700

    using test branch to try using fetch

--- src/main.js
+++ src/main.js
@@ -4,21 +4,16 @@ import './css/styles.css';
 import $ from 'jquery';
 import CurrencyAPI from './js/currency';
 
+function clearFields() {
+  $("#dollarAmount").val("");
+  $("#currencyType").val("");
+}
+
+function // put stuff here. 
+
+
+
+
 $(document).ready(function() {
-  $('#convertCurrencyBtn').click(function(){
-    let dollarAmount = parseInt($('#dollarAmount').val()); 
-    $('#convertedCurrencyOutput').text(dollarAmount); // This just returns the dollar amount. 
-    
-    let selectedCurrency = $('#selectedCurrency').val(); // console returns undefined. 
-
-    let currencyPromise = CurrencyAPI.convertCurrency(selectedCurrency);
-    currencyPromise.then(function (response) {
-
-      const body = JSON.parse (response);
-      $("#convertCurrencyOutput").html(body.conversion_rates.selectedCurrency);
-      return body;
-    }, function (error) {
-      console.error('request error ', error);
-    }); dollarAmount; 
-  });
+  
 });
