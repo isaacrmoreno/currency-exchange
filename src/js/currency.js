ad8686274e826d52258e4e34499bec746d7da606
Isaac Moreno <ipdxcreative@gmail.com>
2021-04-25 19:55:06 -0700

    going to attempt to use fetch versus XMLHTTPRequest

--- src/js/currency.js
+++ src/js/currency.js
@@ -1,20 +1,14 @@
 export default class CurrencyAPI {
-  static convertCurrency(selectedCurrency) { // put currency in here? 
-    return new Promise(function (resolve, reject) {
-      let request = new XMLHttpRequest();
-      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${selectedCurrency}`;
-
-      request.onload = function () {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject (request.response);
+  static convertCurrency() { 
+    return fetch(`https://v6.exchangerate-api.com/v6/?${process.env.API_KEY}/latest/USD`)
+      .then(function(response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
         }
-      };
-      request.open('GET', url, true);
-      request.send();
-    });
+        return response.json();
+      })
+      .catch(function(error) {
+        return error;
+      });
   }
 }
-
-
