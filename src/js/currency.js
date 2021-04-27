f9ec738f806ca84b611ff46f19b2ed54fe55614e
Isaac Moreno <ipdxcreative@gmail.com>
2021-04-26 20:23:26 -0700

    small typo fix

--- src/js/currency.js
+++ src/js/currency.js
@@ -1,7 +1,7 @@
 export default class CurrencyAPI {
   static convertCurrency() { 
     return fetch(`https://v6.exchangerate-api.com/v6/?${process.env.API_KEY}/latest/USD`)
-      .then(function(response) {
+      .then(function (response) {
         if (!response.ok) {
           throw Error(response.statusText);
         }
