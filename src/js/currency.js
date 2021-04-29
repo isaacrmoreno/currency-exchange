e8614e90e8876b17f83e288400725ecc16399f40
Isaac Moreno <ipdxcreative@gmail.com>
2021-04-28 19:38:54 -0700

    refactored

--- src/js/currency.js
+++ src/js/currency.js
@@ -1,6 +1,6 @@
 export default class CurrencyAPI {
-  static convert(baseCurrency, targetCurrency) { 
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${baseCurrency}/${targetCurrency}`)
+  static convert(baseCurrency, targetCurrency, conversionRate) { 
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${baseCurrency}/${targetCurrency}/${conversionRate}`)
       .then((response) => {
         if (!response.ok) {
           throw Error(response.statusText);
